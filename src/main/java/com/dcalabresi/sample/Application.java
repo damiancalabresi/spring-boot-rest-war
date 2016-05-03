1a50283a5dc6c8f96465a7566e1d7c974cf1ded2
Calabresi <dcalabre@am.sony.com>
2016-05-03 11:32:01 -0700

    Initial commit

---
+++ src/main/java/com/dcalabresi/sample/Application.java
@@ -0,0 +1,24 @@
+package com.dcalabresi.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/dcalabresi/sample/Greeting.java
@@ -0,0 +1,24 @@
+package com.dcalabresi.sample;
+
+/**
+ * Created by dcalabre on 5/3/2016.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
